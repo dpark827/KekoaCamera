13b94d7fa92f91925ff7282dd1fd1a9c7291bcdb
dpark827 <33493986+dpark827@users.noreply.github.com>
2018-02-12 16:40:27 +0000

    Update and rename SigghtseeingGide.py to SightseeingGidee.py

--- SightseeingGidee.py
+++ SightseeingGidee.py
@@ -1,21 +1,18 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-
-places = {'shrine': (0,50,0), 
-         'lake': (23,34,-12),
-         'mountian': (-40,41,32),
-         'river': (8,31,-42)}
-
+places = {'lake': (0,5,0), 
+         'river': (32,-43,21),
+         'valley': (-4,-14,-23),
+         'statue': (8,13,24)}
 choice = " "
-
 while choice != "exit":
-    choice = input("Enter a location, type exit to close | ")
+    choice = input("Enter a location or you can Enter exit to close : ")
     if choice in places:
         location = places[choice]
-        
+
         x = location[0]
         y = location[1]
         z = location[2]
-        
+         
         mc.player.setTilePos(x, y, z) 
